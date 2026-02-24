c8fdc91ce9a8c137ddc1541df946aa15777d873f
dwow213 <judahmanoscaleal@gmail.com>
2026-02-23 22:49:31 -0500

    asking questions work
    currently replies only show in console, starting to work on making it show in a textbox and trait menu

--- RambunctiousRonnieGameJam/Assets/Scripts/EventCore.cs
+++ RambunctiousRonnieGameJam/Assets/Scripts/EventCore.cs
@@ -6,4 +6,11 @@ public class EventCore : MonoBehaviour
     [HideInInspector]
     //event for creating a character, also randomizing their appearance and traits
     public UnityEvent createNewCharacterEV;
+
+    [HideInInspector]
+    //event for asking questions, which will get a reply from character and update traits
+    public UnityEvent<Question> askQuestionEV;
+
+    //function that will invoke askQuestionEV. for the question buttons
+
 }
