ef1cceaf1f6664f7f8ba0039e9037de07a79d683
valor87 <134859690+valor87@users.noreply.github.com>
2026-02-25 13:56:06 -0500

    cleaning up some code and adding in comments

--- RambunctiousRonnieGameJam/Assets/Scripts/RemovedLimbsManager.cs
+++ RambunctiousRonnieGameJam/Assets/Scripts/RemovedLimbsManager.cs
@@ -7,26 +7,21 @@ public class RemovedLimbsManager : MonoBehaviour
     public int LimbsOwned;
     public GameObject Instantiate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SeveredLimb(GameObject Limb)
     {
+        // dont let the player have more than 3 limbs
         if(LimbsOwned >= 3)
             return;
+        
         limbsOwned.Add(Limb);
+        // how many limbs the player has
         LimbsOwned++;
         Vector3 pos = gameObject.transform.Find($"Spot {LimbsOwned}").transform.position;
+        // create the limb for the player to move around
         GameObject StoredLimb = Instantiate(Instantiate, pos, Quaternion.identity);
         StoredLimb.tag = Limb.tag;
+        // set the limb type and limb enum to the one that the player removed
         StoredLimb.GetComponent<LimbClassification>().Limb = Limb.GetComponent<LimbClassification>().Limb;
         StoredLimb.GetComponent<LimbClassification>().LimbType = Limb.GetComponent<LimbClassification>().LimbType;
     }
--- RambunctiousRonnieGameJam/Assets/Scripts/SelectingLimb.cs
+++ RambunctiousRonnieGameJam/Assets/Scripts/SelectingLimb.cs
@@ -1,21 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SelectingLimb : MonoBehaviour
 {
+    [Header("The limb and type the player is looking at")]
     public Limb LookingAtLimb;
     public LimbCharacter LookingAtLimbType;
+
+    [Header("Raycast layers")]
     public LayerMask CharacerLayer;
     public LayerMask ItemLayer;
 
     public RemovedLimbsManager RemovedLimbs;
     public LimbClassification LimbClass;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
@@ -25,11 +23,14 @@ public class SelectingLimb : MonoBehaviour
 
     void MoveRayCast()
     {
+        // setting values
         Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         Debug.DrawRay(MouseRay.origin, Vector3.forward, Color.green);
         Limb hoverLimb = Limb.None;
         LimbCharacter hoverLimbType = LimbCharacter.None;
+
+
         if (Physics.Raycast(MouseRay, out hit, 100))
         {
             LimbClass = hit.collider.gameObject.GetComponent<LimbClassification>();
@@ -41,25 +42,22 @@ public class SelectingLimb : MonoBehaviour
             hoverLimbType = LimbClass.LimbType;
             LimbClass.Hover = true;
 
+            // the player removes a limb from the body of the character
             if (Input.GetMouseButtonDown(0))
             {
                 if (hit.collider.gameObject.layer == 6)
                 {
 
-                    PlayerInteraction(hit.collider.gameObject);
+                    RemoveLimbInteraction(hit.collider.gameObject);
                 }
                 
             }
-
+            // the plaeyr is moving the ui limb
             if (Input.GetMouseButton(0))
             {
-                if (hit.collider.gameObject.layer == 7)
-                {
-
-                    hit.collider.gameObject.transform.position
-                        = new Vector3(hit.point.x, hit.point.y, hit.collider.gameObject.transform.position.z);
-                }
+                moveLimbFromInventory(hit.collider.gameObject, hit.point);
             }
+            // setting the enum of what the player is looking at
             LookingAtLimb = hoverLimb;
             LookingAtLimbType = hoverLimbType;
         }
@@ -72,12 +70,20 @@ public class SelectingLimb : MonoBehaviour
         }
     }
 
-    void PlayerInteraction(GameObject LC)
+    void moveLimbFromInventory(GameObject LC, Vector3 hit)
+    {
+        if (LC.layer == 7)
+        {
+
+            LC.transform.position = new Vector3(hit.x, hit.y, LC.transform.position.z);
+        }
+    }
+    void RemoveLimbInteraction(GameObject LC)
     {
         StartCoroutine(RemoveLimb(LC));
         LC.GetComponent<LimbClassification>().playRemoveAnimation();
     }
-
+    // wait for the animation to finish
     IEnumerator RemoveLimb(GameObject LC)
     {
         yield return new WaitForSeconds(1);
